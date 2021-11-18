0d8df1634c8716d03cfbf929c6e1d0482fb4a03f
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-18 15:40:15 +0100

    A finir !!

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
+let barriere = document.getElementById('viewport');
+let kenny = document.getElementById('kenny');
 
-
-document.getElementById('up').addEventListener('click',function() {
+document.getElementById('up').addEventListener('click', function() {
     let t = parseInt(document.getElementById('kenny').style.top);
     t = t - 10;
     document.getElementById('kenny').style.top = t + 'px';
 });
+
+document.getElementById('left').addEventListener('click', function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t - 10;
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('right').addEventListener('click', function() {
+    let t = parseInt(document.getElementById('kenny').style.left);
+    t = t + 10;
+    document.getElementById('kenny').style.left = t + 'px';
+});
+
+document.getElementById('down').addEventListener('click', function() {
+    let t = parseInt(document.getElementById('kenny').style.top);
+    t = t + 10;
+    document.getElementById('kenny').style.top = t + 'px';
+});
+
+let width = parseInt(barriere.style.width = 500 + 'px');
+if (kenny === width) {
+    width = 0;
+}
+
+let deadZone = document.createElement('div');
+deadZone.className = 'deadZone';
+deadZone.style.position = "absolute";
+deadZone.style.bottom = "250px";
+deadZone.style.width = '50px';
+deadZone.style.backgroundColor = "red";
+deadZone.style.height = '100px';
+barriere.append(deadZone);
+
+if ( deadZone === parseInt(50 + 'px')){
+    kenny.style.top = 200 + "px";
+    kenny.style.left = 200 + "px";
+    alert("Kenny est décédé..... Bravo !");
+}
+
+
